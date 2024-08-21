18f9932dbf8f51c5a3777f371b1fec8d3a3f88c6
Shoeb3084 <m.shoeb.q.3084@gmail.com>
2024-08-21 09:42:27 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,10 @@
-# this file is created to test the functionality of the github Actions.
-# app.py
+# This file is created to test the functionality of the github Actions.
+# app.py 
 # This is a test commit
+# Addition function 
 def add(a, b):
     return a + b
-
+# Test addition function 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
